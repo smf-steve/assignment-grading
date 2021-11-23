ede25c92b48359edc94052a85bd56c4168a5a100
Steve Fitzgerald <steve@csun.edu>
2021-11-22 19:04:04 -0800

    more cleanup work

--- grade.bash
+++ grade.bash
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-# This script files defines a number of functions to faciliate
+# This script files defines a number of functions to facilitate
 # the grading of student assignments submitted via GitHub Classroom.
 
-# Theses assignments are presumed to be paper-based like assignments within a text file.
-# Hence, a visual review of each SUBMISSION_FILE is neeeded.  It is also presumed that
+# These assignments are presumed to be paper-like assignments within a text file.
+# Hence, a visual review of each SUBMISSION_FILE is needed.  It is also presumed that
 # each line that contains an answer includes the ANSWER_TAG that allows for
-# the collection of just the answers via a simple 'grep' of the SUBMISSION_FILE
+# the collection of just the answers via a simple 'grep' of the SUBMISSION_FILE.
 
 # The appropriate file is opened for visual review, and then, via the CLI, the professor is 
 # prompted for a score for each individual element in the scoring guide (rubric)
@@ -16,24 +16,26 @@
 # Classroom Setup Process
 #   - create a github organization
 #   - create a github classroom
+#   - install this repository
 #   - updated the classroom.env file
+#   - source grade.bash <class-directory>
 
 # Assignment Grading Setup Process
 #   - publish via github classroom the assignment with the name "<assignment-name>"
-#   - git clone the template assignment which ahs the name "<assignment-name>"
+#   - git clone the template assignment which has the name "<assignment-name>"
 #   - cd "<assignment-name>"
 #   - create an "assignment.env" file to override the defaults (optional)
 #   - create the answers.md file
 #   - create the rubric.grading file
 
-# Assignment Grading Process
+# Per Assignment Grading Process
 #   - cd "<assignment-name>"
-#   - source grade_start
+#   - grade_start
 #   - reset_grading (optional)
 #   - clone_submissions
 #   - grade_submissions
 #   - publish_grades
-#   * recode the grades: "insert grades.<assignment>" into the master spreedsheet
+#   * record the grades: "insert grades.<assignment>" into the master spreadsheet
 
 # Assignment re-grading of single student process
 #   - cd "<assignment-name>"
@@ -41,37 +43,43 @@
 #   - pull_submission "<account>"
 #   - grade_submission "<account>"
 #   - post_submission "<account>"
-#   * recode the grades: "insert grades.<assignment>" into the master spreedsheet
+#   * update the individual grade within the master spreadsheet
 
 #
 #############################################################
 
-GRADE_SCRIPT="${CLASSROOM_DIR}/grade.bash"  # This is the name of this particular script 
+if [[ -z ${CLASSROOM_DIR} ]] ; then
+  CLASSROOM_DIR=$1
+fi
+
+GRADING_SCRIPT="${CLASSROOM_DIR}/grade.bash"  # This is the name of this particular script 
 CLASSROOM_ENV="${CLASSROOM_DIR}/classroom.env"
-ASSIGNMENT_ENV=${PWD}/default-assignment.env
+ASSIGNMENT_ENV="${CLASSROOM_DIR}/default-assignment.env"
 source ${CLASSROOM_ENV}
 source ${ASSIGNMENT_ENV}
 
-# grade_start is presume to be executed within the top-level directory of a particular assignment
-grade_start () {
-  CLASSROOM_DIR="${PWD}/../"
+# grade_start must be called at the top-level directory of a particular assignment
+function grade_start () {
+  CLASSROOM_DIR="${PWD}/.."
   source ${GRADING_SCRIPT}
-  [[ -f ${ASSIGNMENT_DIR}/assignment.env ]] && {
-    ASSIGNMENT_ENV=${ASSIGNMENT_DIR}/assignment.env
+
+  # If there is a per-assignment assignment.env file, allow that one to override the default
+  if [[ -f "${ASSIGNMENT_DIR}/assignment.env" ]] ; then
+    ASSIGNMENT_ENV="${ASSIGNMENT_DIR}/assignment.env"
     source ${ASSIGNMENT_ENV}
-  } # If there is a per-assignment assignment.env allow that one to override the default
+  fi
 }
 
 # Convention Related Files
-CLASS_ROSTER=${CLASS_DIR}/roster                  # List of github usernames of each student
+CLASS_ROSTER="${CLASS_DIR}/roster"                  # List of github usernames of each student
    # Modify the class roster to include: username <e.g. email address>
-CLASS_GRADE_REPORT=${CLASS_DIR}/grades.${ASSIGNMENT_NAME}
-SUBMISSION_DIR=${ASSIGNMENT_DIR}/submissions
+CLASS_GRADE_REPORT="${CLASS_DIR}/grades.${ASSIGNMENT_NAME}"
+SUBMISSION_DIR="${ASSIGNMENT_DIR}/submissions"
 
 # Assignment Based Files
 ASSIGNMENT_FILE="assignment.md"                   # Contained within the student's repo
 SUBMISSION_FILE="submission.md"                   # Contained within the student's repo
-ANSWER_FILE=${ASSIGNMENT_DIR}/answers.md          # To be added to the student's repo
+ANSWER_FILE="${ASSIGNMENT_DIR}/answers.md"        # To be added to the student's repo
 RUBRIC_FILE="${ASSIGNMENT_DIR}/rubric.grading"
 STUDENT_GRADE_REPORT="grade.report"
 
@@ -87,14 +95,18 @@ terminal=$(tty)
 #   - a grade report is created, with the total points tallied
 #   - summary information is provided   
 function grade_submission () {
-  _dir=${SUBMISSION_DIR}/${ASSIGNMENT_PREFIX}-${1}
+  _dir="${SUBMISSION_DIR}/${ASSIGNMENT_PREFIX}-${1}"
 
   (
     cd $_dir  
     rm -f ${STUDENT_GRADE_REPORT}
 
-    grep ${ANSWER_TAG} "${STUDENT_ASSIGNMENT}" > ${STUDENT_ASSIGNMENT}.txt
-    open -a ${GRADING_EDITOR} ${STUDENT_ASSIGNMENT} ${STUDENT_ASSIGNMENT}.txt
+    _files="${STUDENT_ASSIGNMENT}"
+    if [[ $ANSWER_TAG ]] ; then
+      grep ${ANSWER_TAG} "${STUDENT_ASSIGNMENT}" > ${STUDENT_ASSIGNMENT}.txt
+      _files="$_files ${STUDENT_ASSIGNMENT}.txt"
+    fi
+    ${LAUNCH_COMMAND} "${GRADING_EDITOR}" ${_files}
 
     _score=0
     # Add the grad.report prologue
@@ -126,7 +138,7 @@ function grade_submission () {
     printf "$_user: $_score\n" >>${CLASS_GRADE_REPORT}
   )
 }
-function grade_submissions ()
+function grade_submissions () {
   while read _user ; do
     grade_submission ${_user}
   done < ${CLASS_ROSTER}
@@ -134,16 +146,16 @@ function grade_submissions ()
 
 
 function reset_grading () {
-  [[ -f ${CLASS_GRADE_REPORT} ]] && 
-      mv ${CLASS_GRADE_REPORT} ${CLASS_GRADE_REPORT}.$(date "+%Y:%m:%d:%H:%M")
+  [[ -f "${CLASS_GRADE_REPORT}" ]] && 
+    mv ${CLASS_GRADE_REPORT} ${CLASS_GRADE_REPORT}.$(date "+%Y:%m:%d:%H:%M")
   find ${SUBMISSION_DIR} -name ${STUDENT_GRADE_REPORT} -exec git rm -f {} \;
 }
 
 
 function clone_submission () {
-   _dir=${SUBMISSION_DIR}/${ASSIGNMENT_PREFIX}-${1}
+   _dir="${SUBMISSION_DIR}/${ASSIGNMENT_PREFIX}-${1}"
 
-   mkdir "$_dir"
+   mkdir -p "$_dir"
    git -C ${_dir} clone ${REPO_PREFIX}/${ASSIGNMENT_PREFIX}-${_user}.git 
 }
 function clone_submissions () {
@@ -166,11 +178,11 @@ function pull_submissions () {
 function publish_grade () {
   _dir=${SUBMISSION_DIR}/${ASSIGNMENT_PREFIX}-${1}/
 
-  if [[ -f {ANSWER_FILE } ]] {
+  if [[ -f {ANSWER_FILE} ]] ; then
     cp ${ANSWER_FILE} ${_dir}/.
     git -C ${_dir} add ${ANSWER_FILE}
     git -C ${_dir} commit -m 'Added Answers File' ${ANSWER_FILE}
-  }
+  fi
   git -C ${_dir} add ${STUDENT_GRADE_REPORT}
   git -C ${_dir} commit -m 'Added Student Grade Report' ${STUDENT_GRADE_REPORT}
   git -C ${_dir} push
--- sample_assignment/README.md
+++ sample_assignment/README.md
@@ -1,10 +1,4 @@
-# Quiz #2: Digital Logic
-
-### Date: November 8, 2021
-### Allocated time:  1.5 hours (with a 10 minute cushion)
-### Exam Times
-* Morning Class:  9:05am -- 10:45  
-* Afternoon Class:  2:05pm -- 3:45
+# General Assignment Directions
 
 ## To Get Started:
   1. Clone this repository to your computer.  
@@ -18,15 +12,15 @@
 ## Work on the Assignment
   1. Incremently edit and submit your "submission.md" file
      - ``git add``, ``git commit``, and ``git push``
-     - waiting to the end of the alloted time to do your first ``git push`` could result in an incomplement submission
+     - get into the able of making incrementy updates to your working assignments.
   2. Your answers need to be positioned in the file correctly.
-     - provide an answer to every line that contains an "answer" comment.
-     - placement of your answer needs to be to the left of the "answer" comment.
+     - provide an answer to every line that contains an "answer" HTML comment.
+     - placement of your answer needs to be to the left of the "answer" HTML comment.
   3. Use additional spacing to ensure your answer is _easy_ to read.
   4. Add additional information as you feel is necessary.
 
 ## Grading
-   1. An automated process will be used to expidate the grading of this test. 
+   1. An automated process will be used to expidate the grading of this assignment. 
    2. As such, it is important that you position your answers appropriately. 
    3. A file called "answers.md" will be provided to you with all the correct answers.
    4. A file called "grade.report" will be provided with your final score and any breakdown.
