2cf619a6fc0383fbfebb58fb45c16e49ab54e04f
Steve Fitzgerald <steven.fitzgerald@my.csun.edu>
2023-03-11 12:41:10 -0800

    played with ENV varialbes associated with files that control submission dates, e.g., DUE_DATE   ensured consistent referencing of names   ensured variables are appropriate set   defined ENV variables via a cat of the associated file
    realigned the output of the statitics file

--- bin/git.statistics.bash
+++ bin/git.statistics.bash
@@ -3,7 +3,7 @@
 
 trap 'rm .$$_full.log' 0
 # due_date="Nov 12 00:00:00"  # "+%b %d %T"
-# timelimit="2H"  # 120M
+# time_limit="2H"  # 120M
 # grace_period="2M"   # 2 minutes
 
 RELEASE_DATE_FILE="${ASSIGNMENT_DIR}/release_date"
@@ -16,8 +16,8 @@ STUDENT_STAT_REPORT="statistics.report"
 RELEASE_DATE="Not Defined"
 ACCEPT_DATE="Not Defined"
 DUE_DATE="Not Defined"
-TIME_LIMIT="Not Defined"
-GRACE_PERIOD=
+TIME_LIMIT=
+GRACE_PERIOD="0S"
 TIMELIMIT_DUE_DATE=
 
 # Algorithm:
@@ -27,23 +27,15 @@ TIMELIMIT_DUE_DATE=
 #    - ACCEPT_INFO <-- last line
 #    - LAST_COMMIT_INFO <-- first line
 # 3. Determine version_TS
-#    - If no set due-date nor timelimit, then LAST_COMMIT_INFO
+#    - If no set due_date nor time_limit, then LAST_COMMIT_INFO
 #    - due_date_TS
 #      * date -j -f "%b %d %T" "$(due_date)" "+%s"
-#    - time_limit_TS = accept_date + timelimit
-#      * date -r <ts> -v +"$timelimit"
+#    - time_limit_TS = accept_date + time_limit
+#      * date -r <ts> -v +"$time_limit"
 #    - version_TS = min(due_date_TS, time_limit_TS)
 # 4. Extract SUMBISSION INFO
 # 5. Determine the number of commits
 # 6. Output the information
-if [[ -f "${RELEASE_DATE_FILE}" ]] ; then
-   RELEASE_DATE="$(cat ${RELEASE_DATE_FILE})"
-fi
-
-GRACE_PERIOD="0S"
-if [[ -f "${GRACE_PERIOD_FILE}" ]] ; then
-   GRACE_PERIOD="$(cat ${GRACE_PERIOD_FILE})"
-fi
 
 # Read git log
 git log --format='format:%h %ct' > .$$_full.log
@@ -54,19 +46,29 @@ LAST_COMMIT_INFO=( $(head -1 .$$_full.log) )
 
 
 # Determine SUBMISSION INFO
-# # presume no due date or timelimit
+# # presume no due date or time_limit
 SUBMISSION_INFO=( ${LAST_COMMIT_INFO[@]} )
 
 DUE_DATE_TS="${SUBMISSION_INFO[1]}"
 TIME_LIMIT_TS="${SUBMISSION_INFO[1]}"
 
-if [[ -f "${DUE_DATE_FILE}" ]] ; then
-  DUE_DATE="$(cat ${DUE_DATE_FILE})"
+
+### These values are set via grade.bash
+if [[ -s "${RELEASE_DATE_FILE}" ]] ; then
+   RELEASE_DATE="$(cat_nocomments ${RELEASE_DATE_FILE})"
+fi
+
+if [[ -s "${GRACE_PERIOD_FILE}" ]] ; then
+   GRACE_PERIOD="$(cat_nocomments ${GRACE_PERIOD_FILE})"
+fi
+
+if [[ -s "${DUE_DATE_FILE}" ]] ; then
+  DUE_DATE="$(cat_nocomments ${DUE_DATE_FILE})"
   DUE_DATE_TS=$( date -j -f '%b %d %T' -v +${GRACE_PERIOD} "${DUE_DATE}" '+%s' )
 fi
 
-if [[ -f "${TIME_LIMIT_FILE}" ]] ; then
-  TIME_LIMIT="$(cat ${TIME_LIMIT_FILE})"
+if [[ -s "${TIME_LIMIT_FILE}" ]] ; then
+  TIME_LIMIT="$(cat_nocomments ${TIME_LIMIT_FILE})"
   TIME_LIMIT_TS=$( date -r ${ACCEPT_INFO[1]} -v +${TIME_LIMIT} -v +${GRACE_PERIOD} '+%s' )
 fi
 
@@ -122,42 +124,76 @@ fi
 MINUTES_LATE="$(( (${LAST_COMMIT_INFO[1]} - ${TIMELIMIT_DUE_DATE_TS}) / 60 ))"
    # Number of minutes the last commit was made after the final due date
 if (( MINUTES_LATE <= 0 )) ; then
-   MINUTES_LATE="Not applicable"
+   MINUTES_LATE=""
 else
    MINUTES_LATE="${MINUTES_LATE} minutes late."
 fi
 
+
+if [[ -z "${TIME_LIMIT}" ]] ; then
+  DUE_DATE="${DUE_DATE}"
+else
+  ACCEPT_DATE="$(date -r ${ACCEPT_INFO[1]} '+%b %d %T')"
+  CUTOFF_DATE="${DUE_DATE}"
+  DUE_DATE="${TIMELIMIT_DUE_DATE}"
+fi
+
 cat > ${STUDENT_STAT_REPORT} <<EOF
-# ASSIGNMENT STATISTICS REPORT
+# Git Statistics Report
+# Github Account: ${_student}
+# Assignment: "${ASSIGNMENT_NAME}"
+# Assignment ID: "${ASSIGNMENT_ID}"
+# --- Due Date:        "${DUE_DATE}"
+# --- Submission Date: "${SUBMISSION_DATE}"
+# --- Tag & Hash:      "${SUBMISSION_TAG} (${SUBMISSION_HASH})"
 
 STATUS="${STATUS}"
-MINUTES_LATE="${MINUTES_LATE}"
+EOF
+if [[ ${STATUS} != "Submission On Time" ]] ; then
+  echo MINUTES_LATE=\"${MINUTES_LATE}\"
+fi >> ${STUDENT_STAT_REPORT} 
+if [[ "${GRACE_PERIOD}" != "None Given" ]] ; then
+ echo "GRACE_PERIOD=\"${GRACE_PERIOD}\""
+fi >> ${STUDENT_STAT_REPORT}
 
-RELEASE_DATE="${RELEASE_DATE}"
 
-ACCEPT_HASH="${ACCEPT_INFO[0]}"
-ACCEPT_TS="${ACCEPT_INFO[1]}"
-ACCEPT_DATE="$(date -r ${ACCEPT_INFO[1]} '+%b %d %T')"
+if [[ -z "${TIME_LIMIT}" ]] ; then
+  echo
+  echo "DUE_DATE=\"${DUE_DATE}\""
+else
+  echo
+  echo "ACCEPT_DATE=\"${ACCEPT_DATE}\""
+  echo " TIME_LIMIT=\"${TIME_LIMIT}\""
+  echo "   DUE_DATE=\"${DUE_DATE}\""
+  echo CUTOFF_DATE=\"${CUTOFF_DATE}\"
+fi >> ${STUDENT_STAT_REPORT} 
 
-GRACE_PERIOD="${GRACE_PERIOD}"
-DUE_DATE="${DUE_DATE}"
-TIME_LIMIT="${TIME_LIMIT}"
 
-TIMELIMIT_DUE_DATE="${TIMELIMIT_DUE_DATE}"
+cat >> ${STUDENT_STAT_REPORT} <<EOF
 
-SUBMISSION_HASH="${SUBMISSION_HASH}"
-SUBMISSION_TS="${SUBMISSION_TS}"
-SUBMISSION_DATE="${SUBMISSION_DATE}"
-
-LAST_COMMIT_HASH="${LAST_COMMIT_INFO[0]}"
-LAST_COMMIT_TS="${LAST_COMMIT_INFO[1]}"
-LAST_COMMIT_DATE="$(date -r ${LAST_COMMIT_INFO[1]} '+%b %d %T')"
+TAGS=(
+$(git tag)
+)
 
 NUM_COMMITS="${NUM_COMMITS}"
 NUM_COMMITS_POST_SUBMISSION="${NUM_COMMITS_POST_SUBMISSION}"
 NUM_COMMITS_PRE_SUBMISSION="${NUM_COMMITS_PRE_SUBMISSION}"
 
-TAGS=( $(git tag) )
+LAST_COMMIT_HASH="${LAST_COMMIT_INFO[0]}"
+LAST_COMMIT_TS="${LAST_COMMIT_INFO[1]}"
+LAST_COMMIT_DATE="$(date -r ${LAST_COMMIT_INFO[1]} '+%b %d %T')"
+
+SUBMISSION_HASH="${SUBMISSION_HASH}"
+SUBMISSION_TS="${SUBMISSION_TS}"
+SUBMISSION_DATE="${SUBMISSION_DATE}"
+
+ACCEPT_HASH="${ACCEPT_INFO[0]}"
+ACCEPT_TS="${ACCEPT_INFO[1]}"
+ACCEPT_DATE="$(date -r ${ACCEPT_INFO[1]} '+%b %d %T')"
+
 EOF
+
+
+source ${STUDENT_STAT_REPORT}
  
 
